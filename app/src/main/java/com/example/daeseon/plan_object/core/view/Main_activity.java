1710edaa846280bf76f6c5eb558b095b2e3c4d0a
DaeSeon <askyclear2@gmail.com>
2017-03-11 12:54:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/daeseon/plan_object/core/view/Main_activity.java
@@ -0,0 +1,16 @@
+package com.example.daeseon.plan_object.core.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.daeseon.plan_object.R;
+
+public class Main_activity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity);
+    }
+}
