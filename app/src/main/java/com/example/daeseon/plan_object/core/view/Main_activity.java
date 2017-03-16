4033be2e034c6b4ddcc7be856111e03bcd36e80d
DaeSeon Kim <askyclear2@gmail.com>
2017-03-16 12:02:16 +0900

    Bind

--- app/src/main/java/com/example/daeseon/plan_object/core/view/Main_activity.java
+++ app/src/main/java/com/example/daeseon/plan_object/core/view/Main_activity.java
@@ -2,15 +2,23 @@ package com.example.daeseon.plan_object.core.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.daeseon.plan_object.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class Main_activity extends Activity {
 
+
+    @BindView(R.id.txt_1)
+    TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity);
+        ButterKnife.bind(this);
     }
 }
